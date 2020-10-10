8a1288f35829675b5b2b5b174c1fadd2bb3b998b
allgreenL <66295940+allgreenL@users.noreply.github.com>
2020-10-10 14:05:31 +0800

    Update x_assemble_artifact.sh

--- x_assemble_artifact.sh
+++ x_assemble_artifact.sh
@@ -2,6 +2,7 @@ gzip friendlywrt-*/out/*.img
 rm -rf ./artifact/ && mkdir -p ./artifact/
 mv friendlywrt-*/out/*img* ./artifact/
 cp friendlywrt-*/friendlywrt/.config ./artifact/
+cp -r friendlywrt-*/friendlywrt/bin/  ./artifact/
 cd ./artifact/
 md5sum *img* > md5sum.txt
 cd ..
